976db63d55d385bb5367a1087b9c9b12f9b1c946
Abdousimo <96550431+Abdousimo@users.noreply.github.com>
2023-07-17 23:31:44 +0000

    mdification in styling

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -17,16 +17,16 @@ const  Items = ({data,setter,courant,mode}) => {
   }
 
     return (
-        <section className={`h-[50vh] w-full max-w-[350px] sm:max-w-[600px] md:max-w-[800px] overflow-auto rounded-t-lg py-5 ${mode ? 'bg-[#3C303C]' : 'bg-white'}`}>
+        <section className={`h-[50vh] w-full max-w-[350px] sm:max-w-[600px] md:max-w-[800px] overflow-auto rounded-t-lg py-5 ${mode ? 'bg-[#2e2c2e]' : 'bg-white'}`}>
                 {  data.map((item,index) => (
                     <div key={index} className='w-full h-10 px-4 py-8 flex items-center justify-between border-b-2'>
                         <div className='flex text-center gap-8'>
-                            <button onClick={()=> handleComplited(index)} className={`${mode ? 'text-white' : ''}`}>
+                            <button onClick={()=> handleComplited(index)} className={`${mode ? 'text-gray-300' : ''}`}>
                                 {!item.isComplited ? <BiCheckbox size={35}/> : <BiCheckboxChecked size={35}/>}
                             </button>
-                            <p className={`text-xl ${mode ? 'text-white' : ''}`}>{item.name}</p>
+                            <p className={`text-xl ${mode ? 'text-gray-300' : ''}`}>{item.name}</p>
                         </div>
-                        <button className={mode ? 'text-white' : ''} onClick={()=> handleDelete(index)}>
+                        <button className={mode ? 'text-gray-300' : ''} onClick={()=> handleDelete(index)}>
                            <AiOutlineClose size={25}/>
                         </button>
                     </div>
