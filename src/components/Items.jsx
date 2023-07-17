e26d9027f1da788d53d28ddc22dd89a3b2c3483f
Abdousimo <96550431+Abdousimo@users.noreply.github.com>
2023-07-17 22:55:11 +0000

    initial commit

---
+++ src/components/Items.jsx
@@ -0,0 +1,40 @@
+import React, { useState,useEffect } from 'react';
+import {AiOutlineClose} from 'react-icons/ai'
+import {BiCheckboxChecked,BiCheckbox} from 'react-icons/bi'
+
+const  Items = ({data,setter,courant,mode}) => {
+
+ function handleComplited(id) {
+    const newArray = [...data]
+    newArray[id].isComplited = !(newArray[id].isComplited)
+    setter(newArray)
+  }
+
+  function handleDelete(id){
+    const newArray = [...data]
+    newArray.splice(id,1)
+    setter(newArray)
+  }
+
+    return (
+        <section className={`h-[50vh] w-full max-w-[350px] sm:max-w-[600px] md:max-w-[800px] overflow-auto rounded-t-lg py-5 ${mode ? 'bg-[#3C303C]' : 'bg-white'}`}>
+                {  data.map((item,index) => (
+                    <div key={index} className='w-full h-10 px-4 py-8 flex items-center justify-between border-b-2'>
+                        <div className='flex text-center gap-8'>
+                            <button onClick={()=> handleComplited(index)} className={`${mode ? 'text-white' : ''}`}>
+                                {!item.isComplited ? <BiCheckbox size={35}/> : <BiCheckboxChecked size={35}/>}
+                            </button>
+                            <p className={`text-xl ${mode ? 'text-white' : ''}`}>{item.name}</p>
+                        </div>
+                        <button className={mode ? 'text-white' : ''} onClick={()=> handleDelete(index)}>
+                           <AiOutlineClose size={25}/>
+                        </button>
+                    </div>
+                ))}
+
+                
+        </section>
+    );
+}
+
+export default Items;
